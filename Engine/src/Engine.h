2e8bb33bb6c75441b631848a43d346a160dd0965
Dominik Baczyński <dombacy@gmail.com>
2020-05-22 19:29:05 +0200

    Setup

---
+++ Engine/src/Engine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//For use by Engine applications
+
+#include "Engine/Application.h"
+
+
+//----------------------------
+#include "Engine/EntryPoint.h"
+//----------------------------
